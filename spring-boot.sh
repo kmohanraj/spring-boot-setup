feb98e361fc5aeebcf878a09b8dff908aefccd08
kmohanraj <mohanksv.89@gmail.com>
2022-11-10 12:55:41 +0530

    Update spring-boot.sh

--- spring-boot.sh
+++ spring-boot.sh
@@ -24,7 +24,12 @@ if [ "$machine" = "Mac" ] ; then
   echo "${green} Spring Boot Installation Complete ${reset} "
   echo "===================================================="
   # brew install homebrew/cask-versions/adoptopenjdk8 --cask
-  asdf plugin-add java
+  # asdf plugin-add java
+  asdf plugin-add java https://github.com/halcyon/asdf-java.git
+  # asdf install java adoptopenjdk-11.0.15+10 -> working
+  # asdf global java adoptopenjdk-11.0.15+10 -> working
+  # . ~/.asdf/plugins/java/set-java-home.bash -> working
+  # . ~/.asdf/plugins/java/set-java-home.zsh -> working   
   asdf install java adoptopenjdk$OPEN_JDK_VERSION
   asdf global java adoptopenjdk$OPEN_JDK_VERSION
 
